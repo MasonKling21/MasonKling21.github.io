e317a84d37d1f63ed4d8844873e60ed1420a6b6a
MasonKling21 <56097078+MasonKling21@users.noreply.github.com>
2025-01-31 09:28:13 -0800

    Update Technologies.jsx

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,274 +1,107 @@
-import { useState } from 'react'
-import styled from 'styled-components'
-import { DiReact, DiPython, DiJavascript1, DiNodejs, DiMongodb, DiHtml5, DiCss3, DiJava, DiMysql } from "react-icons/di"
-import { SiCplusplus, SiC, SiWindows, SiSpring } from 'react-icons/si'
+import { useState } from "react";
+import styled from "styled-components";
+import {DiReact, DiPython, DiJavascript1, DiNodejs, DiMongodb, DiHtml5, DiCss3, DiJava, DiMysql} from "react-icons/di";
+import { SiCplusplus, SiC, SiWindows, SiSpring } from "react-icons/si";
 import { FaGit } from "react-icons/fa";
 
-var text = "Hover Icon";
+const techList = [
+  { icon: <DiReact />, name: "React" },
+  { icon: <DiPython />, name: "Python" },
+  { icon: <DiJavascript1 />, name: "JavaScript" },
+  { icon: <DiNodejs />, name: "Node.js" },
+  { icon: <DiMongodb />, name: "MongoDB" },
+  { icon: <DiJava />, name: "Java" },
+  { icon: <SiSpring />, name: "Spring" },
+  { icon: <DiMysql />, name: "MySQL" },
+  { icon: <DiHtml5 />, name: "HTML5" },
+  { icon: <DiCss3 />, name: "CSS3" },
+  { icon: <SiCplusplus />, name: "C++" },
+  { icon: <SiC />, name: "C" },
+  { icon: <FaGit />, name: "Git" },
+  { icon: <SiWindows />, name: "Windows" },
+];
 
 const Technologies = () => {
-    document.title = "Technologies";
+  document.title = "Technologies";
 
-    const [hover, setHover] = useState(false);
-
-    const onHover = (e, val) => {
-        setHover(true);
-        text = val;
-    }
-
-    const offHover = (e) => {
-        setHover(false);
-        text = "";
-    }
+  const [hoverText, setHoverText] = useState("Hover over an icon");
 
   return (
     <>
-
-    <CarouselContainer>
+      <CarouselContainer>
         <Carousel>
-            <Track>
-
-            <div className='slide'>
-                <DiReact size="7rem" className="icon" 
-                onMouseEnter={(e) => onHover(e, "React")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiPython size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Python")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiJavascript1 size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "JavaScript")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiNodejs size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Node.js")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiMongodb size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "MongoDB")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiJava size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Java")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <SiSpring size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Spring")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiMysql size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "MySQL")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiHtml5 size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "HTML5")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiCss3 size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "CSS3")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <SiCplusplus size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "C++")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <SiC size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "C")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <FaGit size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Git")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <SiWindows size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Windows")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-
-            <div className='slide'>
-                <DiReact size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "React")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiPython size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Python")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiJavascript1 size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "JavaScript")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiNodejs size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Node.js")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiMongodb size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "MongoDB")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiJava size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Java")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <SiSpring size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Spring")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiMysql size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "MySQL")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiHtml5 size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "HTML5")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <DiCss3 size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "CSS3")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <SiCplusplus size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "C++")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <SiC size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "C")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <FaGit size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Git")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            <div className='slide'>
-                <SiWindows size="7rem" className="icon"
-                onMouseEnter={(e) => onHover(e, "Windows")}
-                onMouseLeave={(e) => offHover(e)}
-                />
-            </div>
-            </Track>
+          <Track>
+            {[...techList, ...techList].map((tech, index) => (
+              <Slide key={index}
+                onMouseEnter={() => setHoverText(tech.name)}
+                onMouseLeave={() => setHoverText("Hover over an icon")}
+              >
+                {tech.icon}
+              </Slide>
+            ))}
+          </Track>
         </Carousel>
         <TextContainer>
-        <h1>{ text }</h1>
+          <h1>{hoverText}</h1>
         </TextContainer>
-    </CarouselContainer>
+      </CarouselContainer>
     </>
-  )
-}
+  );
+};
 
 const TextContainer = styled.div`
-width: 300px;
-height: 64px;
-text-align: center;
-border-radius: 50px;
-display: flex;
-align-items: center;
-justify-content: center;
-color: white;
-background: linear-gradient(90deg, rgba(11,131,18,1) 0%, rgba(9,121,92,1) 35%, rgba(0,212,255,1) 100%);
+  width: 300px;
+  height: 64px;
+  text-align: center;
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 1.2rem;
+  background: linear-gradient(90deg, rgba(11,131,18,1) 0%, rgba(9,121,92,1) 35%, rgba(0,212,255,1) 100%);
 `;
 
 const CarouselContainer = styled.div`
-color: white;
-display: grid;
-place-items: center;
-
-.slide {
-    height: 200px;
-    width: 250px;
-    display: flex;
-    align-items: center;
-    padding: 15px;
-    perspective: 100px;
-    margin-left: 10px;
-    margin-right: 10px;
-}
-
-.icon {
-    transition: 0.3s ease;
-    width: 100%;
-}
-
-.icon:hover {
-    transform: translateZ(20px);
-}
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Carousel = styled.div`
-margin: auto;
-position: relative;
-width: 90%;
-display: grid;
-place-items: center;
-overflow: hidden;
+  width: 90%;
+  overflow: hidden;
 `;
 
 const Track = styled.div`
-    display: flex;
-    width: calc(270px * 28);
-    animation: scroll 40s linear infinite;
-    :hover {
-        animation-play-state: paused;
-    }
-    @keyframes scroll {
-        0% {
-            transform: translateX(0);
-        }
-        100% {
-            transform: translateX(calc(-270px * 14));
-        }
-    }
+  display: flex;
+  width: calc(150px * ${techList.length * 2});
+  animation: scroll 30s linear infinite;
+
+  :hover {
+    animation-play-state: paused;
+  }
+
+  @keyframes scroll {
+    0% { transform: translateX(0); }
+    100% { transform: translateX(calc(-150px * ${techList.length})); }
+  }
 `;
 
-export default Technologies
+const Slide = styled.div`
+  height: 150px;
+  width: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 4rem;
+  cursor: pointer;
+  transition: transform 0.3s ease;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+export default Technologies;
